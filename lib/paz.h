21807cbbcb355e213009150fc37f7b52455c194d
RandoMBuilltShit <flimsen1.4@gmail.com>
2026-03-23 17:15:50 +0100

    Delete lib directory

--- lib/paz.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// PAZ library — PAMT archive parsing, ChaCha20 decryption, LZ4 decompression
-
-#include "PazTypes.h"
-#include "PamtFile.h"
-#include "PamtExtractor.h"
-#include "CryptChaCha20.h"
-#include "Lz4.h"
